3df845363fb59263abf9b21bee7a89046000a200
AndreiShishkin <88075071+AndreiShishkin@users.noreply.github.com>
2022-02-16 20:00:40 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using EverlastingTrain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EverlastingTrain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Train tr = new Train();
+            List<Car> cars = new List<Car>();
+            cars = tr.FillingTrain();
+            cars[0].lightIsOn = true;
+
+            int step;
+            for (step = 1;  step < cars.Count; step++)
+            {
+                if (step >= cars.Count)
+                    step = cars.Count - step;
+                if (cars[step].lightIsOn)
+                {
+                cars[step].lightIsOn = false;
+                    if (cars[step].lightIsOn == cars[0].lightIsOn)
+                    {
+                        step++;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(step);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Train.cs
@@ -0,0 +1,27 @@
+﻿using EverlastingTrain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EverlastingTrain
+{
+    internal class Train
+    {
+        public List<Car> FillingTrain()
+        {
+            List<Car> result = new List<Car>();
+            Random rnd = new Random();
+            int count = rnd.Next(0, 100);
+            for (int i = 0; i < count - 1; i++)
+            {
+                Car car = new Car();
+                car.numOfCar = rnd.Next(0, 100);
+                car.lightIsOn = Convert.ToBoolean(rnd.Next(0, 2));
+                result.Add(car);
+            }
+            return result;
+        }
+    }
+}
